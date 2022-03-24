be1b2e8e0adcc6d4ed047f77b0eccb9bb1f91c3d
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-03-24 23:00:44 +0200

    design folder

---
+++ design/js/script.js
@@ -0,0 +1,28 @@
+// let applyMentor = document.getElementById("applyMentor");
+// let login = document.getElementById("login");
+// let signUp = document.getElementById("signUp");
+
+
+// login.addEventListener("click",()=>{
+//     loadPage("login.html");
+//     });
+// applyMentor.addEventListener("click",()=>loadPage("applyMentor.html"));
+// console.log(login)
+// signUp.addEventListener("click",()=>loadPage("signUp.html"));
+
+
+
+
+
+// const loadPage = (page)=>{
+//     var httpRequest= new XMLHttpRequest();
+//     httpRequest.onreadystatechange=function(){
+//         if(this.readyState==4 && this.status==200){
+//             document.getElementById("body").innerHTML=this.responseText;
+//         }
+//     }
+//     httpRequest.open("Get",page,true);
+//     httpRequest.send();
+// }
+
+
