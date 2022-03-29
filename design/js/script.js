c3335d06e77e0a4726d9a3073d78a2233d814403
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-03-30 01:12:37 +0200

    navbar done

--- design/js/script.js
+++ design/js/script.js
@@ -1,28 +1,18 @@
-// let applyMentor = document.getElementById("applyMentor");
-// let login = document.getElementById("login");
-// let signUp = document.getElementById("signUp");
+let menu_btn = document.querySelector("#menu-btn");
+let navbar   = document.querySelector(".header nav"); 
+menu_btn.addEventListener('click',()=>{
+    navbar.classList.toggle("d-none");
 
-
-// login.addEventListener("click",()=>{
-//     loadPage("login.html");
-//     });
-// applyMentor.addEventListener("click",()=>loadPage("applyMentor.html"));
-// console.log(login)
-// signUp.addEventListener("click",()=>loadPage("signUp.html"));
+});
 
 
 
 
 
-// const loadPage = (page)=>{
-//     var httpRequest= new XMLHttpRequest();
-//     httpRequest.onreadystatechange=function(){
-//         if(this.readyState==4 && this.status==200){
-//             document.getElementById("body").innerHTML=this.responseText;
-//         }
-//     }
-//     httpRequest.open("Get",page,true);
-//     httpRequest.send();
-// }
 
 
+
+window.onscroll = () =>{
+    navbar.classList.add("d-none");
+
+};
