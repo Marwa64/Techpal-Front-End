70dfddea929f7489cc64bb61d044f5f9b618c9a5
Marwa64 <farabi.marwa@gmail.com>
2022-07-04 00:40:22 +0200

    fix: add current track name to news

--- src/pages/student/News.js
+++ src/pages/student/News.js
@@ -7,22 +7,23 @@ import { connect } from 'react-redux'
 const API_URL = 'https://newsapi.org/v2/everything?apiKey=4c815a8efe264f8ba724edc7b68523d6&language=en&sortBy=publishedAt'
 
 const News = ({ currentTrack }) => {
+  const [spinner, setSpinner] = useState(false)
   const [news, setNews] = useState([])
   const searchArticle = async (trackName) => {
+    setSpinner(true)
     const response = await fetch(`${API_URL}&q=${trackName}`)
     const data = await response.json()
-    // console.log(data.articles)
     setNews(data.articles)
+    setSpinner(false)
   }
   useEffect(() => {
-    // searchArticle(currentTrack)
-    searchArticle('frontend')
+    searchArticle(currentTrack.name)
   }, [])
 
   return (
 
-    <Layout spinner={false} pageName='News'>
-      <PurpleBar title='News' button={false} />
+    <Layout spinner={spinner} pageName='News'>
+      <PurpleBar title={`News Related To ${currentTrack.name}`} button={false} />
       <div className="container mt-5">
         <div className='row  ps-5 pb-2 pt-2 '>
           <h2 className='h4'>Recommended For You  </h2 >
@@ -32,7 +33,7 @@ const News = ({ currentTrack }) => {
           news?.length > 0
             ? (
             <div className=' container'>
-              { news.map((article) => <NewsCard key={article.auther} article={article} onClick={() => { window.open(article.url, '_blank').focus() }} />) }
+              { news.map((article, index) => <NewsCard key={`${article.auther}${index}`} article={article} onClick={() => { window.open(article.url, '_blank').focus() }} />) }
             </div>)
             : (
                 <>
